2c7abc4d803613765eefd0243b55c09914c18193
Orçun Aşık <asikorcun@gmail.com>
2024-12-23 21:37:35 +0300

    Added DbContext class

---
+++ CorePortfolio.DataAccess/Concrete/Context/MyAppContext.cs
@@ -0,0 +1,21 @@
+﻿using CorePortfolio.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorePortfolio.DataAccess.Concrete.Context;
+public class MyAppContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=CorePortfolioDb;User ID=sa;Password=Password1;");
+    }
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Experience> Experiences { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<MyService> MyServices { get; set; }
+    public DbSet<Portfolio> Portfolios { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+    public DbSet<SocialMedia> SocialMedias { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+}
