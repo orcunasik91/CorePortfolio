93f3b3c1924438138379722870392f5c6659ac86
Orçun Aşık <asikorcun@gmail.com>
2025-01-04 13:49:10 +0300

    Experience and Skill sections's crud operations completed

---
+++ CorePortfolio.WebUI/Controllers/ExperiencesController.cs
@@ -0,0 +1,55 @@
+﻿using CorePortfolio.Business.Abstract;
+using CorePortfolio.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.WebUI.Controllers;
+public class ExperiencesController : Controller
+{
+    private readonly IExperinceService _experinceService;
+
+    public ExperiencesController(IExperinceService experinceService)
+    {
+        _experinceService = experinceService;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var experiences = _experinceService.GetList();
+        return View(experiences);
+    }
+
+    [HttpGet]
+    public IActionResult CreateExperience()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateExperience(Experience experience)
+    {
+        _experinceService.Insert(experience);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult GetExperience(int id)
+    {
+        var experience = _experinceService.GetById(id);
+        return View(experience);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateExperience(Experience experience)
+    {
+        _experinceService.Update(experience);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult RemoveExperience(int id)
+    {
+        var experience = _experinceService.GetById(id);
+        _experinceService.Delete(experience);
+        return RedirectToAction(nameof(Index));
+    }
+}
--- CorePortfolio.WebUI/Controllers/SkillsController.cs
+++ CorePortfolio.WebUI/Controllers/SkillsController.cs
@@ -1,11 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CorePortfolio.Business.Abstract;
+using CorePortfolio.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CorePortfolio.WebUI.Controllers;
 public class SkillsController : Controller
 {
+    private readonly ISkillService _skillService;
 
+    public SkillsController(ISkillService skillService)
+    {
+        _skillService = skillService;
+    }
+
+    [HttpGet]
     public IActionResult Index()
     {
+        var skills = _skillService.GetList();
+        return View(skills);
+    }
+
+    [HttpGet]
+    public IActionResult CreateSkill()
+    {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateSkill(Skill skill)
+    {
+        _skillService.Insert(skill);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult GetSkill(int id)
+    {
+        var skill = _skillService.GetById(id);
+        return View(skill);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateSkill(Skill skill)
+    {
+        _skillService.Update(skill);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult RemoveSkill(int id)
+    {
+        var skill = _skillService.GetById(id);
+        _skillService.Delete(skill);
+        return RedirectToAction(nameof(Index));
     }
 }
