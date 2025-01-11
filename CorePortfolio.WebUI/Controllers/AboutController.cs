9dffc2102b13a7eeb3719e7f5a88160889631da6
Orçun Aşık <asikorcun@gmail.com>
2025-01-11 20:01:25 +0300

    About section completed for AdminLayout

---
+++ CorePortfolio.WebUI/Controllers/AboutController.cs
@@ -0,0 +1,35 @@
+﻿using CorePortfolio.Business.Abstract;
+using CorePortfolio.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.WebUI.Controllers;
+public class AboutController : Controller
+{
+    private readonly IAboutService _aboutService;
+
+    public AboutController(IAboutService aboutService)
+    {
+        _aboutService = aboutService;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var datas = _aboutService.GetList();
+        return View(datas);
+    }
+
+    [HttpGet]
+    public IActionResult GetAbout(int id)
+    {
+        var datas = _aboutService.GetById(id);
+        return View(datas);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateAbout(About about)
+    {
+        _aboutService.Update(about);
+        return RedirectToAction(nameof(Index));
+    }
+}
